583055c5302abcfb28e7af8bcc91d7073f498b8e
rmnieto <rod.martin.nieto@live.com>
2023-12-02 00:36:32 -0500

    Commit first test

---
+++ main.py
@@ -0,0 +1,16 @@
+import flet
+from flet import *
+
+def main(page:Page):
+    appbar = AppBar(
+        title=Text("Flet web app",size=30,color="white"),
+        bgcolor="blue"
+    )
+    content_1=Container(
+        content=Column([
+            Text("Hello From Flet basic ...", size=40)
+        ])
+    )
+    page.add(appbar, content_1)
+
+flet.app(target=main)
